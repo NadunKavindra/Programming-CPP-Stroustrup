4931532d8766bdc85c2b8b9d1550152a3b1c0aab
Mustafa Adaoglu <m.adaoglu@outlook.com>
2021-06-21 13:58:33 +0300

    Coding style improvements

--- Chapter 2/Exercises/exercise1.cpp
+++ Chapter 2/Exercises/exercise1.cpp
@@ -1,7 +1,7 @@
 #include "../../std_lib_facilities.h"
 int main()
 {
-   cout<<"Hello, programming!\n";
-   cout<<"Here we go!\n";
+   cout << "Hello, programming!\n";
+   cout << "Here we go!\n";
    return 0;
 }
