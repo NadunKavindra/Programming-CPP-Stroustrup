2b608f2735541703f4fea808acd929db57a32204
Mustafa Adaoglu <m.adaoglu@outlook.com>
2021-08-26 12:46:29 +0300

    Solution to Exercise 5 Chapter 8

---
+++ Chapter 8/Exercises/exercise5_reverse.cpp
@@ -0,0 +1,81 @@
+/**
+   Exercise 5 on Page 300
+   @file exercise5_reverse.cpp
+   @author Mustafa Adaoglu
+*/
+
+#include "../../std_lib_facilities.h"
+
+/**
+   Prints a vector of ints to cout
+   @param v the vector to be printed
+   @param label used as a "heading" in the output
+*/ 
+void print(const vector<int>& v, string label)
+{
+   cout << label << ": " << "{ ";
+
+   for (int i { 0 }; i < v.size(); ++i) {
+      cout << v[i];
+
+      if (i != v.size() - 1) {
+         cout << ", ";
+      }
+   }
+   cout << " }" << '\n';
+}
+
+/**
+   Reverses the order of elements of vector using pass by value
+   @param v the vector whose elements are to be reversed
+   @returns a new vector with reversed sequence of elements 
+*/
+vector<int> reverse(const vector<int>& v)
+{
+   vector<int> result;
+
+   for (const int element : v) {
+      result.insert(result.begin(), element);
+   }
+
+   // alternatively
+   // for (int i = v.size() - 1; i >= 0; --i) {
+   //    result.push_back(v[i]);
+   // }
+
+   return result;
+}
+
+/**
+   Reverses the order of elements of the provided vector using pass 
+   by reference
+   @param v vector whose elements are to be reversed
+**/
+void reverse_by_ref(vector<int>& v)
+{
+   // swap values from the outside in
+   for (int i { 0 }; i < v.size() / 2; ++i) {
+      swap(v[i], v[v.size() - 1 - i]);
+   }
+}
+
+int main()
+try {
+   vector<int> test_values { 1, 2, 3, 5, 7, 9 };
+   print(test_values, "TEST VALUES");
+
+   print(reverse(test_values), "REVERSED using pass by value");
+
+   reverse_by_ref(test_values);
+   print(test_values, "TEST VALUES REVERSED using pass by reference");
+
+   return 0;
+}
+catch (exception& e) {
+   cerr << "Error: " << e.what() << '\n';
+   return 1;
+}
+catch (...) {
+   cerr << "Error: unknown exception\n";
+   return 2;
+}
