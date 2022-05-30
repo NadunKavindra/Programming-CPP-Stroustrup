acde945cd96144140ee11aa19d9263fe93e142b5
Mustafa Adaoglu <m.adaoglu@outlook.com>
2022-05-30 15:50:16 +0100

    Chapter 11 Exercise 9 solution

---
+++ Chapter11/Exercise9/text_to_binary.cpp
@@ -0,0 +1,76 @@
+/*
+   Chapter 11 Exercise 9 Part 1, Page 409
+   Program that converts an ordinary text file to binary
+   (Using the book's definition of "binary")
+*/
+
+// Need to use this for as_bytes() function
+#include "../../std_lib_facilities.h"
+
+/// A wrapper around the ifstream constructor
+/// @throws runtime_error if file cannot be opened
+void open_file_input(const string& filename, ifstream& ifs, bool binary = false)
+{
+   if (binary) {
+      ifs = ifstream{filename, ios_base::binary};
+   }
+   else {
+      ifs = ifstream{filename};
+   }
+
+   if (!ifs) {
+      throw runtime_error("Cannot open file " + filename);
+   }
+}
+
+/// A wrapper around the ofstream constructor
+/// @throws runtime_error if file cannot be opened/created
+void open_file_output(const string& filename, ofstream& ofs, bool binary = false)
+{
+   if (binary) {
+      ofs = ofstream{filename, ios_base::binary};
+   }
+   else {
+      ofs = ofstream{filename};
+   }
+
+   if (!ofs) {
+      throw runtime_error("Cannot open file " + filename);
+   }
+}
+
+string change_file_extension(const string& filename, const string& new_extension)
+{
+   return filename.substr(0, filename.find_last_of('.') + 1) + new_extension;
+}
+
+int main(int argc, char const* argv[])
+try {
+   if (argc != 2) {
+      cerr << "Incorrect usage.\n"
+           << "Correct Usage: text_to_binary <input_filename>";
+      return 1;
+   }
+
+   ifstream in;
+   open_file_input(argv[1], in);
+
+   const string output_filename = change_file_extension(argv[1], "bin");
+   ofstream out;
+   open_file_output(output_filename, out, true);
+
+   for (string s; getline(in, s);) {
+      s += '\n';
+      out.write(as_bytes(s), s.size());
+   }
+
+   return 0;
+}
+catch (const exception& e) {
+   cerr << "Error: " << e.what() << '\n';
+   return 1;
+}
+catch (...) {
+   cerr << "Oops: unknown exception\n";
+   return 2;
+}
