98dfc60a60b61dbc73ed25e36ba99aca3c6233b8
Mustafa Adaoglu <m.adaoglu@outlook.com>
2022-05-30 16:04:41 +0100

    Chapter 11 Exercise 10 solution

---
+++ Chapter11/Exercise10/split.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+/// @returns a vector of whitespace seperated substrings of the argument s
+std::vector<std::string> split(const std::string& s)
+{
+   std::vector<std::string> split;
+
+   if (s.length() != 0) {
+      std::istringstream stream{s};
+      for (std::string substring; stream >> substring; split.push_back(substring))
+         ;
+   }
+   return split;
+}
+
+int main()
+{
+   const std::string test = "Hello World Let's Try This";
+
+   std::vector<std::string> vs = split(test);
+
+   for (const std::string& s : vs) {
+      std::cout << s << '\n';
+   }
+}
