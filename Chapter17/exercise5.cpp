d1c0a34f77f838f360d0a684ce1cd1e439a9d6ae
Mustafa Adaoglu <m.adaoglu@outlook.com>
2022-06-27 21:30:52 +0300

    Chapter 17 Exercise 5 solution

---
+++ Chapter17/exercise5.cpp
@@ -0,0 +1,57 @@
+/*
+   Chapter 17 Exercise 5, Page 624
+   Write a function, that finds the first occurence
+   of the C-style string x in s
+*/
+#include <exception>
+#include <iostream>
+
+bool equals(const char* s, const char* s2)
+{
+   if (s == s2) {
+      return true;
+   }
+
+   if (s == nullptr || s2 == nullptr) {
+      return false;
+   }
+
+   while (*s && *s2 && *s == *s2) {
+      ++s;
+      ++s2;
+   }
+   return *s2 == 0;
+}
+
+const char* findx(const char* s, const char* x)
+{
+   if (s == nullptr || x == nullptr) {
+      return nullptr;
+   }
+
+   while (*s) {
+      if (equals(s, x)) {
+         return s;
+      }
+      ++s;
+   }
+   return nullptr;
+}
+
+int main()
+try {
+   char s[] = "Hello World";
+   char x[] = "orl";
+
+   std::cout << findx(s, x) << '\n';
+
+   return 0;
+}
+catch (const std::exception& e) {
+   std::cerr << "Error: " << e.what() << '\n';
+   return 1;
+}
+catch (...) {
+   std::cerr << "Error: unknown exception\n";
+   return 2;
+}
